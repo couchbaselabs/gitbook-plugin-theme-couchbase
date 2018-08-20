39bf0f0529f0fc2b1985f2549ef8bab836c2f704
Aaron Benton <bentonam@gmail.com>
2018-08-20 13:34:13 -0400

    🚀 Initial

---
+++ build.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# Cleanup folder
+rm -rf _assets
+
+# Recreate folder
+mkdir -p _assets/website/
+mkdir -p _assets/ebook/
+
+# Compile JS
+browserify src/js/core/index.js | uglifyjs -mc > _assets/website/gitbook.js
+browserify src/js/theme/index.js | uglifyjs -mc > _assets/website/theme.js
+
+# Compile Website CSS
+lessc -clean-css src/less/website.less _assets/website/style.css
+
+# Compile eBook CSS
+lessc -clean-css src/less/ebook.less _assets/ebook/ebook.css
+lessc -clean-css src/less/pdf.less _assets/ebook/pdf.css
+lessc -clean-css src/less/mobi.less _assets/ebook/mobi.css
+lessc -clean-css src/less/epub.less _assets/ebook/epub.css
+
+# Copy fonts
+mkdir -p _assets/website/fonts
+cp -R node_modules/font-awesome/fonts/ _assets/website/fonts/fontawesome/
+
+# Copy icons
+mkdir -p _assets/website/images
+cp -r _img/icons _assets/website/images/icons
+
+# Copy logos
+mkdir -p _assets/ebook/images
+cp _img/couchbase-logo.png _assets/website/images/couchbase-logo.png
+cp _img/couchbase-logo.png _assets/ebook/images/couchbase-logo.png
