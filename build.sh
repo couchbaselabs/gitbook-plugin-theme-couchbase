a9f99828931e191ff71845ce153303faedd0f621
Aaron Benton <bentonam@gmail.com>
2018-08-20 13:54:54 -0400

    📦 Added Assets

--- build.sh
+++ build.sh
@@ -3,32 +3,33 @@
 # Cleanup folder
 rm -rf _assets
 
-# Recreate folder
-mkdir -p _assets/website/
-mkdir -p _assets/ebook/
+# Recreate directories
+mkdir -p _assets/website/images/icons/
+mkdir -p _assets/website/styles/
+mkdir -p _assets/website/js/
+mkdir -p _assets/website/fonts/
+mkdir -p _assets/ebook/styles/
+mkdir -p _assets/ebook/images/
 
 # Compile JS
-browserify src/js/core/index.js | uglifyjs -mc > _assets/website/gitbook.js
-browserify src/js/theme/index.js | uglifyjs -mc > _assets/website/theme.js
+browserify src/js/core/index.js | uglifyjs -mc > _assets/website/js/gitbook.js
+browserify src/js/theme/index.js | uglifyjs -mc > _assets/website/js/theme.js
 
 # Compile Website CSS
-lessc -clean-css src/less/website.less _assets/website/style.css
+lessc -clean-css src/less/website.less _assets/website/styles/site.css
 
 # Compile eBook CSS
-lessc -clean-css src/less/ebook.less _assets/ebook/ebook.css
-lessc -clean-css src/less/pdf.less _assets/ebook/pdf.css
-lessc -clean-css src/less/mobi.less _assets/ebook/mobi.css
-lessc -clean-css src/less/epub.less _assets/ebook/epub.css
+lessc -clean-css src/less/ebook.less _assets/ebook/styles/ebook.css
+lessc -clean-css src/less/pdf.less _assets/ebook/styles/pdf.css
+lessc -clean-css src/less/mobi.less _assets/ebook/styles/mobi.css
+lessc -clean-css src/less/epub.less _assets/ebook/styles/epub.css
 
 # Copy fonts
-mkdir -p _assets/website/fonts
 cp -R node_modules/font-awesome/fonts/ _assets/website/fonts/fontawesome/
 
 # Copy icons
-mkdir -p _assets/website/images
 cp -r _img/icons _assets/website/images/icons
 
 # Copy logos
-mkdir -p _assets/ebook/images
 cp _img/couchbase-logo.png _assets/website/images/couchbase-logo.png
 cp _img/couchbase-logo.png _assets/ebook/images/couchbase-logo.png
