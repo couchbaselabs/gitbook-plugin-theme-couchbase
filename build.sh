b4cb090721eb6b448b076df33f5c79e45f56c585
Aaron Benton <bentonam@gmail.com>
2019-12-09 20:43:21 -0500

    Added Custom Font Support

--- build.sh
+++ build.sh
@@ -15,16 +15,18 @@ browserify src/js/core/index.js | uglifyjs -mc > _assets/website/js/gitbook.js
 browserify src/js/theme/index.js | uglifyjs -mc > _assets/website/js/theme.js
 
 # Compile Website CSS
-lessc -clean-css src/less/website.less _assets/website/styles/site.css
+lessc src/less/website.less _assets/website/styles/site.css
 
 # Compile eBook CSS
-lessc -clean-css src/less/ebook.less _assets/ebook/styles/ebook.css
-lessc -clean-css src/less/pdf.less _assets/ebook/styles/pdf.css
+lessc src/less/ebook.less _assets/ebook/styles/ebook.css
+lessc src/less/pdf.less _assets/ebook/styles/pdf.css
 lessc -clean-css src/less/mobi.less _assets/ebook/styles/mobi.css
 lessc -clean-css src/less/epub.less _assets/ebook/styles/epub.css
 
 # Copy fonts
 cp -R node_modules/font-awesome/fonts/. _assets/website/styles/fonts/fontawesome
+cp -R src/fonts/. _assets/website/styles/fonts
+cp -R src/fonts/. _assets/ebook/styles/fonts
 
 # Copy icons
 cp -R _img/icons _assets/website/images/icons
